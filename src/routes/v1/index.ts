92a4d5a6f1d52408c3a53f3f43376acb5b36e788
Romeno Wenogk Fernando <drf325@nyu.edu>
2022-01-09 16:46:56 -0500

    fix: removed unused code

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
 
-import auth from './auth';
 import items from './items';
-import users from './users';
 
 const router = Router();
 
-router.use('/auth', auth);
-router.use('/users', users);
 router.use('/items', items);
 
 export default router;
